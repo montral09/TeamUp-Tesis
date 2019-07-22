9de2299fa43c6df3bfc2a0496dbeb3380edc473a
Fabiana <f.iturrarte@hotmail.com>
2019-07-21 22:36:15 -0300

    Se crea combo y campo de búsqueda

--- Frontend/webapi2/webapi2/src/containers/App.js
+++ Frontend/webapi2/webapi2/src/containers/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import Login from '../components/Login/Login';
+import Home from '../components/Home/Home';
+import Navigation from '../components/Navigation/Navigation'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
+  return ( 
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path = "/" component = {Home} exact />
+          <Route path = "/Login" component = {Login} />
+        </Switch>
+      </div>
+      </BrowserRouter>
   );
 }
 
