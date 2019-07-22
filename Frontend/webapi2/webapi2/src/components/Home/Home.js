d9b95006ab72dd8b1fd67bf275fec034e627da10
Fabiana <f.iturrarte@hotmail.com>
2019-07-21 22:36:15 -0300

    Se crea combo y campo de búsqueda

--- Frontend/webapi2/webapi2/src/components/Home/Home.js
+++ Frontend/webapi2/webapi2/src/components/Home/Home.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import './Home.css';
+import {Search} from '../Search/Search';
+import {Navigation} from '../Navigation/Navigation';
  
 const home = (props) => {
     return (
-        <Row>
-            <Col md={12}>
-                <div className={'homeText'}>
-                    "WELCOME TO ACCOUNT-OWNER APPLICATION"
-                </div>
-            </Col>
-        </Row>
+        <div >
+            <Navigation/>
+            <Search/>
+        </div>
     )
 }
  
