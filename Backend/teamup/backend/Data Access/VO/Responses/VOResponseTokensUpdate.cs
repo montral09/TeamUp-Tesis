424540a33f08094f2d2199b95dae530392b1b42e
Fitu <f.iturrarte@hotmail.com>
2019-10-03 15:35:27 -0300

    Se sube git ignore

---
+++ Backend/teamup/backend/Data Access/VO/Responses/VOResponseTokensUpdate.cs
@@ -0,0 +1,19 @@
+﻿using backend.Data_Access.VO.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backend.Data_Access.VO
+{
+    public class VOResponseTokensUpdate : VOResponse
+    {
+        public String AccessToken { get; set; }
+        public String RefreshToken { get; set; }
+        public VOResponseTokensUpdate() { }
+        public VOResponseTokensUpdate(string accessToken, string refreshToken)
+        {
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+    }
+}
