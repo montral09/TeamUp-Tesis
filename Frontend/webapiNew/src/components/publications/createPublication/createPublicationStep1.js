fe94fd3bfd2cd591535075ad11ddc2580b1f8240
Fitu <f.iturrarte@hotmail.com>
2019-11-09 11:06:57 -0300

    Connect add to favorites with API + facilities using react-select

--- Frontend/webapiNew/src/components/publications/createPublication/createPublicationStep1.js
+++ Frontend/webapiNew/src/components/publications/createPublication/createPublicationStep1.js
@@ -1,10 +1,36 @@
 import React from 'react';
-
+import Select from 'react-select';
 import {Form, FormGroup, Label, Input} from 'reactstrap';
 
-
 class CreatePublicationStep1 extends React.Component {
+    constructor(props) {
+        super(props);
+        var facilitiesSelectTemp = []
+        this.props.parentState.facilitiesSelect.forEach(element => {
+            let infText = this.props.parentState.facilities.filter( function(fac){
+                return parseInt(fac.Code) == parseInt(element)
+            });
+            facilitiesSelectTemp.push({label : infText[0].Description, value: element});
+            
+        });
+        this.state= {
+            value: facilitiesSelectTemp
+        }
+        this.handleSelectChange = this.handleSelectChange.bind(this)
+
+    }
+
+
+    handleSelectChange(value){
+        this.setState({ value });
+        this.props.onChange({target :{value:value, id:"facilitiesSelect", options : this.props.parentState.facilities}})		
+    }
+    
     render() {
+        const { value } = this.state;
+        const facilitiesAux = this.props.parentState.facilities.map(function(row) {         
+            return { value : row.Code, label : row.Description }
+         })
       if (this.props.parentState.currentStep !== 1) { // Prop: The current step
         return null
       }
@@ -42,11 +68,13 @@ class CreatePublicationStep1 extends React.Component {
             </FormGroup>
             <FormGroup>
                 <Label for="facilitiesSelect" >Infraestructura</Label>
-                <Input type="select" name="facilitiesSelect" id="facilitiesSelect" multiple onChange={this.props.onChange} defaultValue={this.props.parentState.facilitiesSelect}>
-                {this.props.parentState.facilities.map((facility, key) => {
-                    return <option key={key} value={facility.Code}>{facility.Description}</option>;
-                })}
-                </Input>
+                <Select	
+                    isMulti
+					options={facilitiesAux}
+                    value={value}
+                    onChange = {this.handleSelectChange}
+                    removeSelected = {true}
+				/>
             </FormGroup>
         </Form>
       )
