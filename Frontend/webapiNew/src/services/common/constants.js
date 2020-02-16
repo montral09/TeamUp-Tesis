67f260b551dacd55e4b0be7dc5f00eb650af312f
Fitu <f.iturrarte@hotmail.com>
2020-02-16 12:39:43 -0300

    Auto stash before merge of "dev-fabi" and "origin/dev-bruno"

--- Frontend/webapiNew/src/services/common/constants.js
+++ Frontend/webapiNew/src/services/common/constants.js
@@ -2,6 +2,7 @@ export const MAIN_URL = 'http://teamup-001-site1.itempurl.com/';
 //export const MAIN_URL = 'https://localhost:44372/';
 export const MAIN_URL_WEB = 'http://teamup-001-site1.itempurl.com/web/';
 //export const MAIN_URL_WEB = 'http://localhost:3000/';
+
 export const API_CREDENTIALS = 'AIzaSyDwzr7tDCgFaYyiH5mI_NKdOTElxw1lDss';
 export const GMAP_URL = 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=';
 export const MAIN_WEBSITE_NAME = 'TeamUp!';
