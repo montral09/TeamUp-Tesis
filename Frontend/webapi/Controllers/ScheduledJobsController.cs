ba7ccdbc5d766dd2df3c9a6619541c6f9599222c
Fitu <f.iturrarte@hotmail.com>
2020-03-02 20:26:38 -0300

    StorageUtil moved to persistence project

--- Frontend/webapi/Controllers/ScheduledJobsController.cs
+++ Frontend/webapi/Controllers/ScheduledJobsController.cs
@@ -1,9 +1,7 @@
 ﻿using backend.Logic;
-using backend.Data_Access.VO;
 using System;
 using System.Web.Http;
 using System.Web.Http.Cors;
-using backend.Data_Access.VO.Data;
 using backend.Exceptions;
 
 namespace webapi.Controllers
